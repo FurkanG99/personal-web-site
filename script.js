2f73950743325de1cc360edda15595bad58a43f4
FurkanG99 <furkan_gultekin00@hotmail.com>
2023-11-17 16:06:49 +0300

    güncelleme

--- script.js
+++ script.js
@@ -35,3 +35,98 @@ document.getElementById('download-cv').addEventListener('click',function(){
     link.click();
     document.removeChild(link);
 });
+
+
+/*===== SİDE MENU =======*/ 
+
+document.addEventListener('DOMContentLoaded', function () {
+  
+    setActivePage('home');
+
+   
+    var menuItems = document.querySelectorAll('.nav a');
+    menuItems.forEach(function (menuItem) {
+        menuItem.addEventListener('click', function (event) {
+            
+            event.preventDefault();
+
+            
+            var pageId = this.getAttribute('href').substring(1);
+            setActivePage(pageId);
+        });
+    });
+});
+
+function setActivePage(pageId) {
+
+    var menuItems = document.querySelectorAll('.nav a');
+    menuItems.forEach(function (menuItem) {
+        menuItem.classList.remove('active');
+    });
+
+   
+    var activeMenuItem = document.querySelector('.nav a[href="#' + pageId + '"]');
+    if (activeMenuItem) {
+        activeMenuItem.classList.add('active');
+    }
+}
+document.addEventListener('DOMContentLoaded', function () {
+  
+    setActivePage('home');
+
+
+    var menuItems = document.querySelectorAll('.nav a');
+    menuItems.forEach(function (menuItem) {
+        menuItem.addEventListener('click', function (event) {
+          
+            event.preventDefault();
+
+          
+            var pageId = this.getAttribute('href').substring(1);
+
+            
+            setActivePage(pageId);
+
+            
+            scrollToSection(pageId);
+        });
+    });
+});
+
+function setActivePage(pageId) {
+   
+    var menuItems = document.querySelectorAll('.nav a');
+    menuItems.forEach(function (menuItem) {
+        menuItem.classList.remove('active');
+    });
+
+ 
+    var activeMenuItem = document.querySelector('.nav a[href="#' + pageId + '"]');
+    if (activeMenuItem) {
+        activeMenuItem.classList.add('active');
+    }
+}
+
+function scrollToSection(pageId) {
+ 
+    var targetSection = document.getElementById(pageId);
+    if (targetSection) {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+/*  ===== Call =====*/
+document.addEventListener('DOMContentLoaded', function () {
+    var phoneNumberElement = document.getElementById('phoneNumber');
+
+    phoneNumberElement.addEventListener('click', function () {
+        // Telefon numarasını al ve arama işlemini başlat
+        var phoneNumber = this.querySelector('a').getAttribute('href');
+        callPhoneNumber(phoneNumber);
+    });
+});
+
+function callPhoneNumber(phoneNumber) {
+    // Telefon numarasını arama işlemi için yönlendir
+    window.location.href = phoneNumber;
+}
