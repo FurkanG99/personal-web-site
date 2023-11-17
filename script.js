b13f0f43eee0da47b286ec384edcdb15060ed99e
FurkanG99 <furkan_gultekin00@hotmail.com>
2023-11-17 15:24:34 +0300

    cv yükleme

--- script.js
+++ script.js
@@ -22,3 +22,16 @@ function closeColorSelect(){
     document.getElementsByClassName("colorSelectDiv")[0].style.display = "none";
 }
 
+
+/*========= Download Cv ========*/
+
+document.getElementById('download-cv').addEventListener('click',function(){
+    var cvFilePath = '/cv/Furkan-cv.docx';
+    var link = document.createElement('a');
+    link.href = cvFilePath;
+
+    link.download = 'Furkan-cv.docx';
+    document.body.appendChild(link);
+    link.click();
+    document.removeChild(link);
+});
