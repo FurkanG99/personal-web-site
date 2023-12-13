aeb9308763792227e97910d06af53300f20c63ae
FurkanG99 <furkan_gultekin00@hotmail.com>
2023-12-13 14:13:14 +0300

    d

--- splitText.js
+++ splitText.js
@@ -1,25 +1,25 @@
-import { Bounce } from "gsap";
-import  TweenLite  from "gsap/gsap-core";
-import  TweenMax  from "gsap/gsap-core";
-import SplitText from "gsap/src/SplitText";
+// import { Bounce } from "gsap";
+// import  TweenLite  from "gsap/gsap-core";
+// import  TweenMax  from "gsap/gsap-core";
+// import SplitText from "gsap/src/SplitText";
 
-const  TimelineLite  = require("gsap/gsap-core");
+// const  TimelineLite  = require("gsap/gsap-core");
 
-// //! GSAP Animations
+// // //! GSAP Animations
 
 
 
-// let mySplitText = new SplitText("quote", {type:"words,chars"});
-// TweenLite.set("quote",{perspective:400});
-// TweenMax.staggerFrom(mySplitText.chars, 0.8,{opacity:0, scale:0,y:80, rotationX:180, transformOrigin:"0% 50% -50",ease:Back.easeOut},0.01, allDone);
-// function allDone(){
-//     mySplitText.revert();
-// }
+// // let mySplitText = new SplitText("quote", {type:"words,chars"});
+// // TweenLite.set("quote",{perspective:400});
+// // TweenMax.staggerFrom(mySplitText.chars, 0.8,{opacity:0, scale:0,y:80, rotationX:180, transformOrigin:"0% 50% -50",ease:Back.easeOut},0.01, allDone);
+// // function allDone(){
+// //     mySplitText.revert();
+// // }
 
-let tl = new TimelineLite,
-    mySplitText = new SplitText(".splitTexttt", {type:"words, lines, chars"});
-        words = mySplitText.chars
-        tl.staggerFrom(lines, 1, {opacity:0, x:80, ease: Power4.easeOut}, 0.4);
-        document.getElementById("animate").onclick = function(){
-            tl.restart();
-        }
+// let tl = new TimelineLite,
+//     mySplitText = new SplitText(".splitTexttt", {type:"words, lines, chars"});
+//         words = mySplitText.chars
+//         tl.staggerFrom(lines, 1, {opacity:0, x:80, ease: Power4.easeOut}, 0.4);
+//         document.getElementById("animate").onclick = function(){
+//             tl.restart();
+//         }
